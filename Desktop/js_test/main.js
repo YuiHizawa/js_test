6129429dddad49aa731f6f90e999de4e775409d1
檜澤ゆい <hizawayui@gmail.com>
2020-08-03 20:35:06 +0900

    edit

---
+++ Desktop/js_test/main.js
@@ -0,0 +1,12 @@
+const member_3 = {
+  "1kumi": {
+    本題: { height: 179, hobby: "サッカー" },
+    kagawa: { height: 165, hobby: "サッカー" },
+  },
+  "2kumi": {
+    hondasan: { height: 179, hobby: "サッカー" },
+    kagawasan: { height: 165, hobby: "サッカー" },
+  },
+};
+
+console.log(member_3);
